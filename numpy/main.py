3e47266b7c6b6aae311e8bb8142715f4eecfe280
Haofeng Chen <haofengac@gmail.com>
2018-03-17 13:30:36 -0400

    added numpy

---
+++ numpy/main.py
@@ -0,0 +1,93 @@
+from modules import *
+import time, os, argparse
+import cupy as cp
+from mnist import MNIST
+from modules import CapsNet, CapsLoss
+from optim import AdamOptimizer
+    
+def parse_args():
+    """
+    Parse input arguments
+    """
+    parser = argparse.ArgumentParser(description='Cupy Capsnet')
+    parser.add_argument('--bs', dest='bs',
+                      help='batch size',
+                      default='100', type=int)
+    parser.add_argument('--lr', dest='lr',
+                      help='learning rate',
+                      default=1e-2, type=float)
+    parser.add_argument('--opt', dest='opt',
+                      help='optimizer',
+                      default='adam', type=str)
+    parser.add_argument('--disp', dest='disp_interval',
+                      help='interval to display training loss',
+                      default='10', type=int)
+    return args
+
+if __name__ == '__main__':
+    
+    args = parse_args()
+    
+    mnist = MNIST(bs=args.bs, shuffle=True)
+    eye = cp.eye(mnist.num_classes)
+    model = CapsNet()
+
+    criterion = CapsLoss()
+    if args.opt == 'adam':
+        optimizer = AdamOptimizer(lr=args.lr)
+        
+    print('Training started!')
+
+    for epoch in range(num_epochs):
+        start = time.time()
+
+        # train
+        correct = 0
+        for batch_idx, (imgs, targets) in enumerate(mnist.train_dataset):
+            optimizer.step()
+            if imgs.shape[0] != bs:
+                continue
+
+            targets = eye[targets]
+            scores, reconst = model(imgs)
+            loss, grad = criterion(scores, targets, reconst, imgs)
+            model.backward(grad, optimizer)
+
+            classes = cp.argmax(scores, axis=1)
+            predicted = eye[cp.squeeze(classes), :]
+
+            predicted_idx = cp.argmax(predicted, 1)
+            label_idx = cp.argmax(targets, 1)
+            correct = cp.sum(predicted_idx == label_idx)
+
+            # info
+            if batch_idx % args.disp_interval == 0:
+                end = time.time()
+                print("[epoch %2d][iter %4d] loss: %.4f, acc: %.4f%% (%d/%d)" \
+                                % (epoch, batch_idx, loss, 100.*correct/bs, correct, bs))
+
+        # val
+        if epoch % val_epoch == 0:
+            print('Validating...')
+            correct = 0
+            total = 0
+
+            for batch_idx, (imgs, targets) in enumerate(mnist.eval_dataset):
+                if imgs.shape[0] != bs:
+                    continue
+
+                targets = eye[targets]
+                scores, reconst = model(imgs)
+                loss, grad = criterion(scores, targets, reconst, imgs)
+                model.backward(grad, optimizer)
+
+                classes = cp.argmax(scores, axis=1)
+                predicted = eye[cp.squeeze(classes, axis=1), :]
+
+                predicted_idx = cp.argmax(predicted, 1)
+                label_idx = cp.argmax(targets, 1)
+                correct += cp.sum(predicted_idx == label_idx)
+                total += targets.shape[0]
+
+            print("[epoch %2d] val acc: %.4f%% (%d/%d)" \
+                                    % (epoch, 100.*correct/total, correct, total))
---
+++ numpy/mnist.py
@@ -0,0 +1,74 @@
+import time, os
+import numpy as np
+import cupy as cp
+from urllib import request
+import gzip
+import pickle
+
+
+class MNIST:
+    def __init__(self, path='data', bs=1, shuffle=False):
+        self.filename = [
+        ["training_images","train-images-idx3-ubyte.gz"],
+        ["test_images","t10k-images-idx3-ubyte.gz"],
+        ["training_labels","train-labels-idx1-ubyte.gz"],
+        ["test_labels","t10k-labels-idx1-ubyte.gz"]
+        ]
+        self.mean = 0.1307
+        self.std = 0.3081
+        self.num_classes = 10
+        self.bs = bs
+        self.path = path
+        
+        if not os.path.exists(self.path):
+            os.mkdir(self.path)
+        if not os.path.exists(self.path+'/mnist.pkl'):
+            self.download_mnist()
+        self.load(shuffle=shuffle)
+        print('Loading complete.')
+        
+    def download_mnist(self):
+        base_url = "http://yann.lecun.com/exdb/mnist/"
+        for name in self.filename:
+            print("Downloading "+name[1]+"...")
+            request.urlretrieve(base_url+name[1], self.path+'/'+name[1])
+        print("Download complete.")
+        self.save_mnist()
+
+    def save_mnist(self):
+        mnist = {}
+        for name in self.filename[:2]:
+            with gzip.open(self.path+'/'+name[1], 'rb') as f:
+                mnist[name[0]] = ((np.frombuffer(f.read(), np.uint8, offset=16).reshape(-1,28*28))/255.-self.mean)/self.std
+        for name in self.filename[-2:]:
+            with gzip.open(self.path+'/'+name[1], 'rb') as f:
+                mnist[name[0]] = np.frombuffer(f.read(), np.uint8, offset=8)
+        with open(self.path+'/'+"mnist.pkl", 'wb') as f:
+            pickle.dump(mnist,f)
+        print("Save complete.")
+
+    def chunks(self, l):
+        for i in range(0, len(l), self.bs):
+            yield l[i:i + self.bs]
+
+    def load(self, shuffle=False):
+        with open(self.path+"/mnist.pkl",'rb') as f:
+            mnist = pickle.load(f)
+        if shuffle:
+            n = mnist['training_images'].shape[0]
+            idxs = np.arange(n)
+            np.random.shuffle(idxs)
+            mnist['training_images'] = mnist['training_images'].reshape((-1,1,28,28))
+            mnist['training_images'] = list(self.chunks(mnist['training_images'][idxs]))
+            mnist['training_labels'] = list(self.chunks(mnist['training_labels'][idxs]))
+            self.train_dataset = zip(cp.array(mnist['training_images']), cp.array(mnist['training_labels']))
+            
+            n = mnist['test_images'].shape[0]
+            idxs = np.arange(n)
+            np.random.shuffle(idxs)
+            mnist['test_images'] = mnist['test_images'].reshape((-1,1,28,28))
+            mnist['test_images'] = list(self.chunks(mnist['test_images'][idxs]))
+            mnist['test_labels'] = list(self.chunks(mnist['test_labels'][idxs]))
+            self.eval_dataset = zip(cp.array(mnist['test_images']), cp.array(mnist['test_labels']))
+
+            
